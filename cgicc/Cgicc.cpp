8457d3bd4698ac53802aba26b53a6829cc011ae3
roker <roker@pep-project.org>
2014-01-01 00:00:00 +0100

    Version 3.2.12

--- cgicc/Cgicc.cpp
+++ cgicc/Cgicc.cpp
@@ -1,6 +1,6 @@
 /* -*-mode:c++; c-file-style: "gnu";-*- */
 /*
- *  $Id: Cgicc.cpp,v 1.31 2013/12/31 17:33:59 sebdiaz Exp $
+ *  $Id: Cgicc.cpp,v 1.33 2014/01/01 10:14:50 sebdiaz Exp $
  *
  *  Copyright (C) 1996 - 2004 Stephen F. Booth <sbooth@gnu.org>
  *                       2007 Sebastien DIAZ <sebastien.diaz@gmail.com>
@@ -387,7 +387,7 @@ cgicc::Cgicc::parseFormInput(const std::string& data, const std::string &content
       oldPos = ++pos;
       
       // Find the '&' or ';' separating subsequent name/value pairs
-      pos = data.find_first_of(';&', oldPos);
+      pos = data.find_first_of(";&", oldPos);
       
       // Even if an '&' wasn't found the rest of the string is a value
       value = form_urldecode(data.substr(oldPos, pos - oldPos));
@@ -410,12 +410,19 @@ cgicc::Cgicc::parseFormInput(const std::string& data, const std::string &content
     std::string 		bType 	= "boundary=";
     std::string::size_type 	pos 	= content_type.find(bType);
 
+    // Remove next sentence
+    std::string                 commatek=";";
+
     // generate the separators
     std::string sep1 = content_type.substr(pos + bType.length());
+    if (sep1.find(";")!=std::string::npos)
+       sep1=sep1.substr(0,sep1.find(";"));
     sep1.append("\r\n");
     sep1.insert(0, "--");
 
     std::string sep2 = content_type.substr(pos + bType.length());
+    if (sep2.find(";")!=std::string::npos)
+       sep2=sep2.substr(0,sep2.find(";"));
     sep2.append("--\r\n");
     sep2.insert(0, "--");
 
